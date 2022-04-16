5778a23277746211e3ad7e77cfb0515d5b18b3f6
Prince Kumar Kanoujiya <iprincekumark@gmail.com>
2022-04-16 23:28:15 +0530

    Time: 15 ms (68.02%), Space: 20.8 MB (95.41%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int help(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        int lh = help(root->left);
+        int rh = help(root->right);
+        if(lh == -1)
+            return -1;
+        if(rh == -1)
+            return -1;
+        if(1 < abs(lh-rh))
+            return -1;
+        else
+            return 1 + max(lh, rh);
+    }
+    bool isBalanced(TreeNode* root) {
+        return help(root) != -1;
+    }
+    /*
+    int depth(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        int l = depth(root->left), r = depth(root->right);
+        return 1 + max(l,r);
+    }
+    bool isBalanced(TreeNode* root) {
+        if(root == NULL)
+            return true;
+        int l = depth(root->left), r = depth(root->right);
+        if(abs(l-r) <= 1 && isBalanced(root->left) && isBalanced(root->right))
+            return true;
+        else
+            return false;
+    }
+    */
+};

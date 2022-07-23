e11ebc85818cf36cae4afa4293c8ed033f89bfab
Prince Kumar Kanoujiya <iprincekumark@gmail.com>
2022-07-24 01:39:39 +0530

    Time: 6 ms (13.39%), Space: 6.5 MB (10.10%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -14,18 +14,5 @@ public:
         return st.empty();
     }
 
-    /*
-    bool isValid(string s) {
-        stack<char> st;  
-        for(auto i:s)  
-        {
-            if(i=='(' or i=='{' or i=='[') st.push(i); 
-            else  {
-                if(st.empty() or (st.top()=='(' and i!=')') or (st.top()=='{' and i!='}') or(st.top()=='[' and i!=']')) 
-                    return false;
-                st.pop();  
-            }
-        }
-        return st.empty();  
-    }*/
+   
 };

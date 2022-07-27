7cb67a12674eb658316ed6cbb57d06a45566915d
Prince Kumar Kanoujiya <iprincekumark@gmail.com>
2022-07-28 01:56:11 +0530

    Time: 54 ms (75.62%), Space: 18 MB (25.27%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
+    static bool  myfunction(vector<int> &a, vector<int> &b) {
+        return a[1] > b[1];
+    }
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
         int limit=0, totalUnits=0, leftBox=0;
         priority_queue<pair<int, int>> pq;
-        for(auto v: boxTypes) {
-            pq.push({v[1],v[0]});
+        for(auto v:boxTypes) {
+            pq.push({v[1], v[0]});
         }
         while(!pq.empty() && limit<truckSize) {
             if((pq.top().second + limit) < truckSize) {
                 limit += pq.top().second;
-                totalUnits += pq.top().first*pq.top().second;
+                totalUnits += pq.top().first * pq.top().second;
             }
             else {
                 leftBox = truckSize - limit;
-                totalUnits += pq.top().first*leftBox;
+                totalUnits += pq.top().first * leftBox;
                 limit += leftBox;
             }
             pq.pop();

174ffaa0eb6bace797467a397b2a1637d79fdeff
Prince Kumar Kanoujiya <iprincekumark@gmail.com>
2022-07-29 07:55:30 +0530

    Time: 3 ms (90.27%), Space: 7.9 MB (95.56%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
-        int i, j, ln=pattern.size();
         vector<string> ans;
-        for(i=0; i<words.size(); i++) {
-            map<char, char> mp1;
-            map<char, char> mp2;
-            int flag=0;
-            for(j=0; j<ln; j++) {
-                auto it1 = mp1.find(pattern[j]);
-                if(it1 == mp1.end()) 
-                    mp1.insert({pattern[j], words[i][j]});
-                else
-                    if(it1->second != words[i][j]) {
-                        flag=1;
-                        break;
-                    }
-                
-                auto it2 = mp2.find(words[i][j]);
-                if(it2 == mp2.end())
-                    mp2.insert({words[i][j], pattern[j]});
-                else
-                    if(it2->second != pattern[j]) {
-                        flag=1;
-                        break;
-                    }
+        for(auto word:words) {
+            vector<char> pt(26, '*');
+            vector<char> wd(26, '*');
+            int i, flag=0, ln=pattern.size();
+            for(i=0; i<ln; i++) {
+                char p = pattern[i];
+                char w = word[i];
+                if(pt[p -'a'] == '*' && wd[w -'a'] == '*') {
+                    pt[p -'a'] = w;
+                    wd[w -'a'] = p;
+                }
+                else if(!(pt[p-'a'] == w && wd[w-'a'] == p)) {
+                    flag=1;
+                    break;
+                }
             }
             if(!flag)
-                ans.push_back(words[i]);
+                   ans.push_back(word);
         }
         return ans;
     }

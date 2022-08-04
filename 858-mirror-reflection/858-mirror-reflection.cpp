14aa94b9594b27789ab98af1fcf7040afe61caab
Prince Kumar Kanoujiya <iprincekumark@gmail.com>
2022-08-04 06:33:44 +0530

    Time: 6 ms (6.61%), Space: 5.8 MB (62.81%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cpp
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int mirrorReflection(int p, int q) {
-        return (p & -p) > (q & -q) ? 2 : (p & -p) < (q & -q) ? 0:1;
+        int ext=q, reflection=p;
+        while(ext%2==0 && reflection%2==0){
+            ext/=2;
+            reflection/=2;
+        }
+        if(ext%2==0 && reflection%2!=0) return 0;
+        if(ext%2==1 && reflection%2==0) return 2;
+        if(ext%2==1 && reflection%2!=0) return 1;
+        return -1;
     }
 };

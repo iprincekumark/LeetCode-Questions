24d1fe52869ffd1130c198e7c4af8bcd134a4a3a
Prince Kumar Kanoujiya <iprincekumark@gmail.com>
2022-10-21 20:43:17 +0530

    Time: 178 ms (97.36%), Space: 60.1 MB (99.79%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        uint8_t  size = nums.size() - 1;
+        for(uint8_t  i = 0; i < size; ++i)
+        {
+            for(uint8_t  j = i + 1; j <= size; ++j)
+                if((nums[i] == nums[j]) &&  j-i <= k)
+                    return true;
+        }
+        return false;
+    }
+};

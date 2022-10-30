a5e7721372ef16542ef563b9424843be835b6b36
Prince Kumar Kanoujiya <iprincekumark@gmail.com>
2022-10-30 09:35:46 +0530

    Time: 450 ms (29.10%), Space: 36.3 MB (51.87%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,41 +1,63 @@
+int dir [4][2]={{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+int dp[50][50][2000];
+struct Data{
+  int x, y, k, length;
+    Data(int a, int b, int c, int d){
+        x = a;
+        y = b;
+        k = c;
+        length = d;
+    }
+};
 class Solution {
 public:
+    void pre(){
+        for(int i = 0; i < 50; i++){
+            for(int j = 0; j < 50; j++){
+                for(int k = 0; k < 2000; k++){
+                    dp[i][j][k] = INT_MAX;
+                }
+            }
+        }
+    }
+    bool ok(int x, int y, int r, int c){
+        return (x < r && y < c && x >= 0 && y >= 0);
+    }
     int shortestPath(vector<vector<int>>& grid, int k) {
-        int m = grid.size(), n = grid[0].size();
-        
-        vector<vector<int>> obs(m,vector<int>(n,INT_MAX));
-        using ti = tuple<int,int,int>;
-        
-        queue<ti>q ; //x,y,curObs
-        q.push({0,0,0});
-        int dis = 0;
-        int dx[] = {0,0,-1,1};
-        int dy[] = {1,-1,0,0};
-        
+        pre();
+        int r = grid.size();
+        int c = grid[0].size();
+        queue <Data> q;
+        Data root(0, 0, k, 0);
+        q.push(root);
         while(!q.empty()){
-            int size = q.size();
-            while(size--){
-                auto [x,y, curObs] = q.front(); q.pop();
-                if(x==m-1 && y==n-1)
-                    return dis;
-                
-                for(int i = 0; i<4; i++){
-                    int nx = x+dx[i];
-                    int ny = y+dy[i];
-                    
-                    if(nx>=0 && nx<m && ny>=0 && ny<n){
-                        
-                        if(curObs+grid[nx][ny]>k || obs[nx][ny]<=curObs+grid[nx][ny])continue;
-                        q.push({nx,ny, curObs+grid[nx][ny]});
-                        obs[nx][ny] = curObs+grid[nx][ny];
-                        
+            Data node = q.front();
+            q.pop();
+            int x = node.x;
+            int y = node.y;
+            int k = node.k;
+            int length = node.length;
+            if(x == r - 1 && y == c - 1) return length;
+            length++;
+            for(int i = 0; i < 4; i++){
+                int nx = x + dir[i][0];
+                int ny = y + dir[i][1];
+                if(nx == r - 1 && ny == c - 1)return length;
+                if(ok(nx, ny, r, c)){
+                    if(grid[nx][ny] == 0){
+                          if(length < dp[nx][ny][k]){
+                              q.push(Data(nx, ny, k, length));
+                              dp[nx][ny][k] = length;
+                          }
+                    }else{
+                        if(k > 0 && length < dp[nx][ny][k]){
+                             q.push(Data(nx, ny, k - 1, length));
+                             dp[nx][ny][k] = length;
+                          }
                     }
                 }
-                
             }
-            dis++;
         }
-        
         return -1;
     }
 };

996505071a626f8b7837e28203d7c86fd0780887
Prince Kumar Kanoujiya <iprincekumark@gmail.com>
2022-11-23 10:31:14 +0530

    Time: 31 ms (81.77%), Space: 20.4 MB (21.72%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,18 @@
+class Solution 
+{
+public:
+    bool isValidSudoku(vector<vector<char>>& board) 
+    {
+        vector<set<char>> rows(9), cols(9), blck(9);            
+        for (int i = 0; i < 9; i++) 
+            for (int j = 0; j < 9; j++) 
+                if (board[i][j] != '.')
+                {
+                    int d = board[i][j], b = (i/3)*3+j/3;
+                    if (!rows[i].emplace(d).second) return false;   
+                    if (!cols[j].emplace(d).second) return false;   
+                    if (!blck[b].emplace(d).second) return false;  
+                }
+        return true;
+    }
+};
